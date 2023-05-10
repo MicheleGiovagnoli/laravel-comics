6357e11a136f3e55b570747ea3df54bc7c878e84
michele <michele.giovagnoli1@gmail.com>
2023-05-10 17:31:55 +0200

    header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <h1>Stamp i titoli del database</h1>
     @foreach($dati as $element)
         <span>{{$element['title']}}</span>
+        <img src="{{$element['thumb']}}" alt="...">
     @endforeach
 @endsection
     
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,19 @@
 <header>
     <h1>header</h1>
-    <nav>
-        <ul>
-            <li><a href="{{route('home')}}">Home</a></li>
-        </ul>
-    </nav>
+    <header>
+        <nav>
+            <!-- Logo Brand -->
+            <div class="brand">
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="Dc Logo">
+            </div>
+
+            <!-- Links -->
+            <ul class="navbar">
+                <li>
+                    <a class="link" href="">                        
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </header>
